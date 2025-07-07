904d1f6d44e8dfc1b51f18d228d4937b3a4f18bf
Lau-San <lausanch0512@gmail.com>
2025-07-07 18:34:47 -0500

    development: Fixed Navigator breaking fzf-lua

--- home/development/neovim/nvim/lua/plugins/zellij.lua
+++ home/development/neovim/nvim/lua/plugins/zellij.lua
@@ -4,10 +4,10 @@ local navigator = {
         require('Navigator').setup(opts)
     end,
     keys = {
-        { '<C-h>', '<CMD>NavigatorLeft<CR>', mode = { 'n', 't' } },
-        { '<C-j>', '<CMD>NavigatorDown<CR>', mode = { 'n', 't' } },
-        { '<C-k>', '<CMD>NavigatorUp<CR>', mode = { 'n', 't' } },
-        { '<C-l>', '<CMD>NavigatorRight<CR>', mode = { 'n', 't' } },
+        { '<C-h>', '<CMD>NavigatorLeft<CR>', mode = { 'n' } },
+        { '<C-j>', '<CMD>NavigatorDown<CR>', mode = { 'n' } },
+        { '<C-k>', '<CMD>NavigatorUp<CR>', mode = { 'n' } },
+        { '<C-l>', '<CMD>NavigatorRight<CR>', mode = { 'n' } },
     },
 }
 
